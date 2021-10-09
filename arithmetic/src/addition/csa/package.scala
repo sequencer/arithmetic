b34820414a91201c302931abc5f29954a9a1189b
Jiuyang Liu <liu@jiuyang.me>
2021-10-09 10:20:44 +0800

    bug fix for adder

--- arithmetic/src/addition/csa/package.scala
+++ arithmetic/src/addition/csa/package.scala
@@ -5,12 +5,12 @@ import chisel3._
 
 package object csa {
   def apply[T <: Data](csa: CSACompressor, width: Option[Int] = None)(in: Vec[T]) = {
-    val m = new CarrySaveAdder(csa, width.getOrElse(in.flatMap(_.widthOption).max))
-    m.in := VecInit(in.map(_.asUInt()))
+    val m = Module(new CarrySaveAdder(csa, width.getOrElse(in.flatMap(_.widthOption).max)))
+    m.in := VecInit(in.map(_.asUInt))
     m.out
   }
 
-  def c22[T <: Data](in: Vec[T]) = apply(CSACompressor2_2, Some(2))(in)
-  def c32[T <: Data](in: Vec[T]) = apply(CSACompressor3_2, Some(3))(in)
-  def c53[T <: Data](in: Vec[T]) = apply(CSACompressor5_3, Some(5))(in)
+  def c22[T <: Data](in: Vec[T]): Vec[UInt] = apply(CSACompressor2_2)(in)
+  def c32[T <: Data](in: Vec[T]): Vec[UInt] = apply(CSACompressor3_2)(in)
+  def c53[T <: Data](in: Vec[T]): Vec[UInt] = apply(CSACompressor5_3)(in)
 }
