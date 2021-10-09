9a792f25245738c3b5797edc5c4058aa62e0dd8a
SharzyL <me@sharzy.in>
2021-10-09 18:46:36 +0800

    unsigned version of Booth recoder.

--- arithmetic/src/multiplier/Booth.scala
+++ arithmetic/src/multiplier/Booth.scala
@@ -2,38 +2,54 @@ package multiplier
 
 import chisel3._
 import chisel3.util.experimental.decode.{TruthTable, decoder}
-import chisel3.util.{BitPat, isPow2, log2Ceil}
+import chisel3.util.{Cat, BitPat, isPow2, log2Ceil}
 
 class Booth(width: Int)(radix: Int) extends Module {
   assert(isPow2(radix))
   val radixLog2 = log2Ceil(radix)
   val input = IO(Input(UInt(width.W)))
-  val output = IO(Output(Vec(Math.ceil(width.toDouble / radixLog2).toInt, UInt((radixLog2 + 1).W))))
+  val output = IO(Output(Vec(
+    (width - 1) / radixLog2 + 1, // = ceil(width / radixLog2)
+    UInt((radixLog2 + 1).W)
+  )))
 
-  Seq.tabulate(output.size) {
-    case i if i <= radixLog2                 =>
-    case width @ i if i >= width - radixLog2 =>
-    case i => decoder(input(radixLog2 * (i + 1) - 1, radixLog2 * i - 1), TruthTable(
+  val paddingLeftWidth = radixLog2 - 1 - (width - 1) % radixLog2
+  val paddedInput = if (paddingLeftWidth == 0)
+    Cat(input, 0.U(1.W))
+  else {
+    Cat(0.U(paddingLeftWidth.W), input, 0.U(1.W))
+  }
+
+  val boothEncodingCoeff = Seq.tabulate(radixLog2 + 1) {
+    case i if i == radixLog2 => -(1 << (radixLog2 - 1))
+    case i if i == 0 => 1
+    case i => 1 << (i - 1)
+  }
+  println(boothEncodingCoeff)
+
+  val boothEncodingTable = TruthTable(
+    Seq
+      .tabulate(1 << (radixLog2 + 1)) { i =>
         Seq
-          .tabulate(2 << (radixLog2 + 1)) { i =>
-            Seq
-              .tabulate(radixLog2 + 1)((bit: Int) => if (BigInt(i).testBit(bit)) 1 else 0)
-              .zip(Seq.tabulate(radixLog2 + 1) {
-                case i if i == 0         => -(2 << (radixLog2 - i - 1))
-                case i if i == radixLog2 => 1
-                case _                   => 2 << (radixLog2 - i - 1)
-              })
-              .map {
-                case (a, b) => a * b
-              }
-              .sum
-          }
-          .zipWithIndex
+          .tabulate(radixLog2 + 1)((bit: Int) => if (BigInt(i).testBit(bit)) 1 else 0)
+          .zip(boothEncodingCoeff)
           .map {
-            case (o, i) =>
-              (BitPat(i.U), BitPat(o.U))
-          },
-        BitPat.dontCare(radixLog2 + 1)
-      ))
+            case (a, b) => a * b
+          }
+          .sum
+      }
+      .zipWithIndex
+      .map {
+        case (o, i) =>
+          val w = (radixLog2 + 1).W
+          (BitPat(i.U(w)), BitPat((o + (1 << (radixLog2 - 1))).U(w)))
+      },
+    BitPat.dontCare(radixLog2 + 1)
+  )
+  println(boothEncodingTable)
+  printf("%b", paddedInput)
+
+  output := Seq.tabulate(output.size) { i =>
+    decoder(paddedInput(radixLog2 * (i + 1), radixLog2 * i), boothEncodingTable)
   }
 }
