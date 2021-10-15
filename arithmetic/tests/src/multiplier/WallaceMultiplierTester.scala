2a55aa5a4ae1858884eddf93b1334e5e4c700362
SharzyL <me@sharzy.in>
2021-10-15 20:23:04 +0800

    refac: change UInt to SInt in multiplier

--- arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
+++ arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
@@ -1,16 +1,23 @@
 package multiplier
 
 import chisel3._
-import chisel3.tester.{testableData, ChiselUtestTester}
+import chisel3.tester.{ChiselUtestTester, testableClock, testableData}
+import chiseltest.simulator.{VerilatorBackendAnnotation, WriteVcdAnnotation}
 import utest._
 
 object WallaceMultiplierTester extends TestSuite with ChiselUtestTester {
   val tests: Tests = Tests {
     test("3*2 on Wallace") {
-      testCircuit(new WallaceMultiplier(4)()) { dut =>
-        dut.a.poke(3.U)
-        dut.b.poke(2.U)
-        println(dut.z.peek())
+      val width = 6;
+      testCircuit(new WallaceMultiplier(width)(), Seq()) { dut =>
+        for (i <- -5 until 5) {
+          for (j <- -5 until 5) {
+            dut.a.poke(i.S)
+            dut.b.poke(j.S)
+            dut.z.peek()
+            dut.clock.step()
+          }
+        }
       }
     }
   }
