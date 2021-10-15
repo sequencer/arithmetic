4f0a0744086db3dd37ed42010b13dc152bf71e58
SharzyL <me@sharzy.in>
2021-10-15 22:43:37 +0800

    refac: more functional way to generat partial sum

--- arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
+++ arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
@@ -1,21 +1,21 @@
 package multiplier
 
+import addition.prefixadder.common.BrentKungSum
 import chisel3._
 import chisel3.tester.{ChiselUtestTester, testableClock, testableData}
-import chiseltest.simulator.{VerilatorBackendAnnotation, WriteVcdAnnotation}
 import utest._
 
 object WallaceMultiplierTester extends TestSuite with ChiselUtestTester {
   val tests: Tests = Tests {
     test("3*2 on Wallace") {
       val width = 6;
-      testCircuit(new WallaceMultiplier(width)(), Seq()) { dut =>
+      testCircuit(new WallaceMultiplier(width)()) { dut =>
         for (i <- -5 until 5) {
           for (j <- -5 until 5) {
             dut.a.poke(i.S)
             dut.b.poke(j.S)
-            dut.z.peek()
             dut.clock.step()
+            dut.z.expect((i * j).S)
           }
         }
       }
