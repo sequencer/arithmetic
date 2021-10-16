d95a5b80d2cccbae625a5284ef1e4784fedc5ff4
Jiuyang Liu <liu@jiuyang.me>
2021-10-16 17:36:06 +0800

    use upstream verify function

--- arithmetic/tests/src/addition/AdderSuite.scala
+++
@@ -1,7 +0,0 @@
-package addition
-
-import formal.FormalSuite
-
-trait AdderSuite extends FormalSuite {
-  def formalFullAdder(dut: () => FullAdder, name: String) = formal(dut, name, success, 0, Nil)
-}
--- arithmetic/tests/src/addition/prefixadder/AdderSpecTester.scala
+++ arithmetic/tests/src/addition/prefixadder/AdderSpecTester.scala
@@ -1,24 +1,25 @@
 package addition.prefixadder
 
-import addition.AdderSuite
 import addition.prefixadder.common.{BrentKungAdder, KoggeStoneAdder, RippleCarry3Adder, RippleCarryAdder}
+import chiseltest.formal.BoundedCheck
+import formal.FormalSuite
 import utest._
 
-object AdderSpecTester extends AdderSuite {
+object AdderSpecTester extends FormalSuite {
   val width = 8
   // TODO: utest only support static test(cannot use map function), maybe we can try to use macro to implement this.
   val tests: Tests = Tests {
     test("ripple carry should pass") {
-      formalFullAdder(() => new RippleCarryAdder(width), "ripple_carry")
+      verify(new RippleCarryAdder(width), Seq(BoundedCheck(1)))
     }
     test("ripple carry 3 fan-in should pass") {
-      formalFullAdder(() => new RippleCarry3Adder(width), "ripple_carry3")
+      verify(new RippleCarry3Adder(width), Seq(BoundedCheck(1)))
     }
     test("kogge stone should pass") {
-      formalFullAdder(() => new KoggeStoneAdder(width), "kogge_stone")
+      verify(new KoggeStoneAdder(width), Seq(BoundedCheck(1)))
     }
     test("brent kung should pass") {
-      formalFullAdder(() => new BrentKungAdder(width), "ripple_carry3")
+      verify(new BrentKungAdder(width), Seq(BoundedCheck(1)))
    }
   }
 }
