4667c175f37a0f9b8bfb00d1423612bfbee55bd3
SharzyL <me@sharzy.in>
2021-10-17 11:53:55 +0800

    feat: support both signed and unsigned multiplier

--- arithmetic/src/multiplier/Multiplier.scala
+++ arithmetic/src/multiplier/Multiplier.scala
@@ -2,13 +2,24 @@ package multiplier
 
 import chisel3._
 
-trait Multiplier extends Module {
+trait Multiplier[T] extends Module {
   val width: Int
-  // TODO: add backpressure for in order signals?
-  val stage: Int
   require(width > 0)
+  val a: T
+  val b: T
+  val z: T
+}
+
+trait SignedMultiplier extends Multiplier[SInt] {
   val a: SInt = IO(Input(SInt(width.W)))
   val b: SInt = IO(Input(SInt(width.W)))
   val z: SInt = IO(Output(SInt((2 * width).W)))
+  assert(a * b === z)
+}
+
+trait UnsignedMultiplier extends Multiplier[UInt] {
+  val a: UInt = IO(Input(UInt(width.W)))
+  val b: UInt = IO(Input(UInt(width.W)))
+  val z: UInt = IO(Output(UInt((2 * width).W)))
   assert(a * b === z)
 }
--- arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
+++ arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
@@ -4,16 +4,25 @@ import chiseltest.formal.BoundedCheck
 import formal.FormalSuite
 import utest._
 
-object FormalWallaceTester extends FormalSuite {
+object WallaceMultiplierTester extends FormalSuite {
   val tests: Tests = Tests {
-    test("wallace multiplier 1") {
-      verify(new WallaceMultiplier(1)(), Seq(BoundedCheck(1)))
+    test("wallace signed multiplier 1") {
+      verify(new SignedWallaceMultiplier(1)(), Seq(BoundedCheck(1)))
     }
-    test("wallace multiplier 4") {
-      verify(new WallaceMultiplier(4)(), Seq(BoundedCheck(1)))
+    test("wallace signed multiplier 4") {
+      verify(new SignedWallaceMultiplier(4)(), Seq(BoundedCheck(1)))
     }
-    test("wallace multiplier 7") {
-      verify(new WallaceMultiplier(7)(), Seq(BoundedCheck(1)))
+    test("wallace signed multiplier 7") {
+      verify(new SignedWallaceMultiplier(7)(), Seq(BoundedCheck(1)))
+    }
+    test("wallace unsigned multiplier 1") {
+      verify(new UnsignedWallaceMultiplier(1)(), Seq(BoundedCheck(1)))
+    }
+    test("wallace unsigned multiplier 4") {
+      verify(new UnsignedWallaceMultiplier(4)(), Seq(BoundedCheck(1)))
+    }
+    test("wallace unsigned multiplier 7") {
+      verify(new UnsignedWallaceMultiplier(7)(), Seq(BoundedCheck(1)))
     }
   }
 }
