c78653269d23ac0adf69b4aaf001c6d68c123d09
GH Cheng <1536771081@qq.com>
2021-12-31 14:02:40 +0800

    adder hasSign into package object prefixsdder

--- arithmetic/src/addition/prefixadder/package.scala
+++ arithmetic/src/addition/prefixadder/package.scala
@@ -7,21 +7,28 @@ import utils.{extend, sIntToBitPat}
 
 package object prefixadder {
 
-  def apply(prefixSum: PrefixSum, width: Option[Int] = None)(a: UInt, b: UInt, cin: Bool = false.B) = {
+  def apply(
+    prefixSum: PrefixSum,
+    width:     Option[Int] = None,
+    hasSign:   Boolean = false
+  )(a:         UInt,
+    b:         UInt,
+    cin:       Bool = false.B
+  ) = {
     val Width = width.getOrElse(Seq(a, b).flatMap(_.widthOption).max)
     val m = Module(new PrefixAdder(Width, prefixSum))
-    // Todo : if Sign extend(,,true), else extend(,,false)?
-    m.a := extend(a, Width, false)
-    m.b := extend(b, Width, false)
+    m.a := extend(a, Width, hasSign)
+    m.b := extend(b, Width, hasSign)
     m.cin := cin
     Cat(m.cout, m.z)
   }
 
-  def brentKun(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None) = apply(BrentKungSum, width)(a, b, cin)
+  def brentKun(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(BrentKungSum, width, hasSign)(a, b, cin)
 
-  def koggeStone(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None) =
-    apply(KoggeStoneSum, width)(a, b, cin)
+  def koggeStone(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(KoggeStoneSum, width, hasSign)(a, b, cin)
 
-  def rippleCarry(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None) =
-    apply(RippleCarrySum, width)(a, b, cin)
+  def rippleCarry(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(RippleCarrySum, width, hasSign)(a, b, cin)
 }
