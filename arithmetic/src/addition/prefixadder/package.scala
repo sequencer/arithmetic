b07c58e3c4f406fcaa4aab9db3b74ef4946ee11f
GH Cheng <1536771081@qq.com>
2022-01-03 12:28:51 +0800

    rm FixexPointedMultiplier & debug

--- arithmetic/src/addition/prefixadder/package.scala
+++ arithmetic/src/addition/prefixadder/package.scala
@@ -2,18 +2,33 @@ package addition
 
 import addition.prefixadder.common.{BrentKungSum, KoggeStoneSum, RippleCarrySum}
 import chisel3._
+import chisel3.util.Cat
+import utils.{extend, sIntToBitPat}
 
 package object prefixadder {
-  def apply(prefixSum: PrefixSum, width: Option[Int] = None)(a: UInt, b: UInt) = {
-    val m = Module(new PrefixAdder(width.getOrElse(Seq(a, b).flatMap(_.widthOption).max), prefixSum))
-    m.a := a
-    m.b := b
-    m.z
+
+  def apply(
+    prefixSum: PrefixSum,
+    width:     Option[Int] = None,
+    hasSign:   Boolean = false
+  )(a:         UInt,
+    b:         UInt,
+    cin:       Bool = false.B
+  ) = {
+    val w = width.getOrElse(Seq(a, b).flatMap(_.widthOption).max)
+    val m = Module(new PrefixAdder(w, prefixSum))
+    m.a := extend(a, w, hasSign)
+    m.b := extend(b, w, hasSign)
+    m.cin := cin
+    Cat(m.cout, m.z)
   }
 
-  def brentKun(a: UInt, b: UInt, width: Option[Int] = None) = apply(BrentKungSum, width)(a, b)
+  def brentKun(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(BrentKungSum, width, hasSign)(a, b, cin)
 
-  def koggeStone(a: UInt, b: UInt, width: Option[Int] = None) = apply(KoggeStoneSum, width)(a, b)
+  def koggeStone(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(KoggeStoneSum, width, hasSign)(a, b, cin)
 
-  def rippleCarry(a: UInt, b: UInt, width: Option[Int] = None) = apply(RippleCarrySum, width)(a, b)
+  def rippleCarry(a: UInt, b: UInt, cin: Bool = false.B, width: Option[Int] = None, hasSign: Boolean = false) =
+    apply(RippleCarrySum, width, hasSign)(a, b, cin)
 }
