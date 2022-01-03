8356683e4ded563190db652a06c997b6f0cb8d4e
GH Cheng <1536771081@qq.com>
2022-01-03 12:28:51 +0800

    rm FixexPointedMultiplier & debug

--- arithmetic/src/multiplier/Multiplier.scala
+++ arithmetic/src/multiplier/Multiplier.scala
@@ -1,25 +1,28 @@
 package multiplier
 
 import chisel3._
+import chisel3.experimental.FixedPoint
 
 trait Multiplier[T] extends Module {
-  val width: Int
-  require(width > 0)
+  val aWidth: Int
+  val bWidth: Int
+  require(aWidth > 0)
+  require(bWidth > 0)
   val a: T
   val b: T
   val z: T
 }
 
 trait SignedMultiplier extends Multiplier[SInt] {
-  val a: SInt = IO(Input(SInt(width.W)))
-  val b: SInt = IO(Input(SInt(width.W)))
-  val z: SInt = IO(Output(SInt((2 * width).W)))
+  val a: SInt = IO(Input(SInt(aWidth.W)))
+  val b: SInt = IO(Input(SInt(bWidth.W)))
+  val z: SInt = IO(Output(SInt((aWidth + bWidth).W)))
   assert(a * b === z)
 }
 
 trait UnsignedMultiplier extends Multiplier[UInt] {
-  val a: UInt = IO(Input(UInt(width.W)))
-  val b: UInt = IO(Input(UInt(width.W)))
-  val z: UInt = IO(Output(UInt((2 * width).W)))
+  val a: UInt = IO(Input(UInt(aWidth.W)))
+  val b: UInt = IO(Input(UInt(bWidth.W)))
+  val z: UInt = IO(Output(UInt((aWidth + bWidth).W)))
   assert(a * b === z)
 }
--- arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
+++ arithmetic/tests/src/multiplier/WallaceMultiplierTester.scala
@@ -7,22 +7,37 @@ import utest._
 object WallaceMultiplierTester extends FormalSuite {
   val tests: Tests = Tests {
     test("wallace signed multiplier 1") {
-      verify(new SignedWallaceMultiplier(1)(), Seq(BoundedCheck(1)))
+      verify(new SignedWallaceMultiplier(1, 1)(), Seq(BoundedCheck(1)))
     }
     test("wallace signed multiplier 4") {
-      verify(new SignedWallaceMultiplier(4)(), Seq(BoundedCheck(1)))
+      verify(new SignedWallaceMultiplier(4, 4)(), Seq(BoundedCheck(1)))
     }
     test("wallace signed multiplier 7") {
-      verify(new SignedWallaceMultiplier(7)(), Seq(BoundedCheck(1)))
+      verify(new SignedWallaceMultiplier(7, 7)(), Seq(BoundedCheck(1)))
     }
+
+    test("wallace signed multiplier 4,7") {
+      verify(new SignedWallaceMultiplier(4, 7)(), Seq(BoundedCheck(1)))
+    }
+    test("wallace signed multiplier 7,4") {
+      verify(new SignedWallaceMultiplier(7, 4)(), Seq(BoundedCheck(1)))
+    }
+
     test("wallace unsigned multiplier 1") {
-      verify(new UnsignedWallaceMultiplier(1)(), Seq(BoundedCheck(1)))
+      verify(new UnsignedWallaceMultiplier(1, 1)(), Seq(BoundedCheck(1)))
     }
     test("wallace unsigned multiplier 4") {
-      verify(new UnsignedWallaceMultiplier(4)(), Seq(BoundedCheck(1)))
+      verify(new UnsignedWallaceMultiplier(4, 4)(), Seq(BoundedCheck(1)))
     }
     test("wallace unsigned multiplier 7") {
-      verify(new UnsignedWallaceMultiplier(7)(), Seq(BoundedCheck(1)))
+      verify(new UnsignedWallaceMultiplier(7, 7)(), Seq(BoundedCheck(1)))
+    }
+
+    test("wallace unsigned multiplier 4,7") {
+      verify(new UnsignedWallaceMultiplier(4, 7)(), Seq(BoundedCheck(1)))
+    }
+    test("wallace unsigned multiplier 7,4") {
+      verify(new UnsignedWallaceMultiplier(7, 4)(), Seq(BoundedCheck(1)))
     }
   }
 }
