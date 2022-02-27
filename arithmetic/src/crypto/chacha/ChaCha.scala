e99c01da64aaf0f6d74a62e062052c4f96a9389d
Jiuyang Liu <liu@jiuyang.me>
2022-02-27 14:58:46 +0800

    wip

--- arithmetic/src/crypto/chacha/ChaCha.scala
+++ arithmetic/src/crypto/chacha/ChaCha.scala
@@ -1,9 +1,10 @@
 package crypto.chacha
 
 import chisel3._
-import chisel3.util.Decoupled
+import chisel3.util.{Decoupled, Mux1H, PopCount}
 
-class ChaCha extends Module {
+case class ChaChaParameter(nonceWidth: Int)
+class ChaCha(parameter: ChaChaParameter) extends Module {
   val head = "expand 32-byte k".grouped(4).map(_.getBytes("ascii"))
 
   // allocate key for this ChaCha Module
@@ -13,7 +14,7 @@ class ChaCha extends Module {
   val key = IO(Input(Vec(2, Vec(4, UInt(32.W)))))
   // Should be ASID tag captured from page info
   // should be an additional userbits in TileLink protocol
-  val nonce = IO(Input(UInt(96.W)))
+  val nonce = IO(Input(UInt(parameter.nonceWidth.W)))
   // key and counter is valid can start to calculate.
   val valid = IO(Input(Bool()))
 
@@ -24,29 +25,91 @@ class ChaCha extends Module {
   }))
 
   // Internal generated nonce which will be used for encrypt and decrypt.
-  val counter = RegInit(0.U(32.W))
+  val counter = RegInit(0.U((128 - parameter.nonceWidth).W))
   val update = RegInit(false.B)
   // nonce update logic
   nonce := Mux(update, counter + 1.U, counter)
 
   val matrix: Seq[Seq[UInt]] = Seq.fill(4)(Seq.fill(4)(RegInit(0.U(32.W))))
 
-  val state = RegInit(0.U(4.W))
-  val flag: Bool = state(0) || state(2)
+  val state = RegInit(1.U(4.W))
+  assert(PopCount(state) === 1.U(4.W), "state must be OneHot.")
+  state := state << 1
 
-  matrix.map { row =>
+  val rounds = RegInit(0.U(5.W))
+  rounds := Mux(rounds === 19.U, 0.U, rounds + 1.U)
+  val control = chisel3.util.experimental.decode.decoder(
+    state,
+    chisel3.util.experimental.decode.TruthTable.fromString(
+      s"""???1->11000100110
+         |??1?->00101001001
+         |?1??->11000010110
+         |1???->00011000000
+         |      ???????????""".stripMargin
+    )
+  )
+  assert(rounds < 20.U, "rounds should less then 20")
+  val flag:           Bool = control(0)
+  val updateA:        Bool = control(1)
+  val updateBShift12: Bool = control(2)
+  val updateBShift7:  Bool = control(3)
+  val updateC:        Bool = control(4)
+  val updateDShift16: Bool = control(5)
+  val updateDShift8:  Bool = control(6)
+  val keepA:          Bool = control(7)
+  val keepB:          Bool = control(8)
+  val keepC:          Bool = control(9)
+  val keepD:          Bool = control(10)
+
+  val permuteOdd:     Bool = rounds(0) && state(3)
+  val permuteEven:    Bool = !rounds(0) && state(3)
+
+  // Data path
+  matrix.foreach { row =>
     val a = row(0)
     val b = row(1)
     val c = row(2)
     val d = row(3)
     val adder = Module(new Add)
     val xor = Module(new Xor)
-
     adder.a := Mux(flag, a, c)
     adder.b := Mux(flag, b, d)
     xor.a := Mux(flag, d, b)
-    xor.b := Mux(flag, a, c)
-
+    xor.b := adder.c
+    a := Mux1H(
+      Map(
+        updateA -> adder.c,
+        permuteOdd -> ???, // matrix()()
+        permuteEven -> ???, // matrix()()
+        keepA -> a
+      )
+    )
+    b := Mux1H(
+      Map(
+        updateBShift12 -> xor.c.rotateLeft(12),
+        updateBShift7 -> xor.c.rotateLeft(7),
+        permuteOdd -> ???, // matrix()()
+        permuteEven -> ???, // matrix()()
+        keepB -> b
+      )
+    )
+    c := Mux1H(
+      Map(
+        updateC -> adder.c,
+        permuteOdd -> ???, // matrix()()
+        permuteEven -> ???, // matrix()()
+        keepC -> c
+      )
+    )
+    d := Mux1H(
+      Map(
+        updateDShift16 -> xor.c.rotateLeft(16),
+        updateDShift8 -> xor.c.rotateLeft(8),
+        permuteOdd -> ???, // matrix()()
+        permuteEven -> ???, // matrix()()
+        keepD -> d
+      )
+    )
   }
 }
 
