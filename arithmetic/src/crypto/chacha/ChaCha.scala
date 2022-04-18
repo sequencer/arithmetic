55492f7f6fe4b9362c452ab5399306c8a7a08cba
Lucas-Wye <lustrew@foxmail.com>
2022-04-18 11:28:05 +0000

    finish chacha

--- arithmetic/src/crypto/chacha/ChaCha.scala
+++ arithmetic/src/crypto/chacha/ChaCha.scala
@@ -1,11 +1,14 @@
 package crypto.chacha
 
 import chisel3._
-import chisel3.util.{Decoupled, Mux1H, PopCount}
+import chisel3.util.{Decoupled, Mux1H}
 
 case class ChaChaParameter(nonceWidth: Int)
 class ChaCha(parameter: ChaChaParameter) extends Module {
-  val head = "expand 32-byte k".grouped(4).map(_.getBytes("ascii"))
+  val chacha_head_1 = 0x61707865.U(32.W)
+  val chacha_head_2 = 0x3320646e.U(32.W)
+  val chacha_head_3 = 0x79622d32.U(32.W)
+  val chacha_head_4 = 0x6b206574.U(32.W)
 
   // allocate key for this ChaCha Module
   // outer nonce should be updated as well.
@@ -20,34 +23,50 @@ class ChaCha(parameter: ChaChaParameter) extends Module {
 
   // used by nonce-pool
   val output = IO(Decoupled(new Bundle {
-    val nonce = Vec(4, UInt(32.W))
-    val x = Vec(4, Vec(4, UInt(32.W)))
+    val nonce = UInt(parameter.nonceWidth.W)
+    val counter = UInt((128 - parameter.nonceWidth).W)
+    val x = Vec(4, Vec(4, UInt(32.W))) // output state matrix
   }))
 
   // Internal generated nonce which will be used for encrypt and decrypt.
   val counter = RegInit(0.U((128 - parameter.nonceWidth).W))
   val update = RegInit(false.B)
-  // nonce update logic
-  nonce := Mux(update, counter + 1.U, counter)
+  // counter update logic
+  counter := Mux(update, counter + 1.U, counter)
+  val last_row = counter ## nonce
 
+  // matrix
   val matrix: Seq[Seq[UInt]] = Seq.fill(4)(Seq.fill(4)(RegInit(0.U(32.W))))
 
-  val state = RegInit(1.U(4.W))
-  assert(PopCount(state) === 1.U(4.W), "state must be OneHot.")
-  state := state << 1
+  val state = RegInit(1.U(5.W))
+  state := Mux(valid, state.rotateLeft(1), state) // rot left circularly
 
   val rounds = RegInit(0.U(5.W))
-  rounds := Mux(rounds === 19.U, 0.U, rounds + 1.U)
+  rounds := Mux(valid && (state(4).asBool), Mux(rounds === 19.U, 0.U, rounds + 1.U), rounds)
   val control = chisel3.util.experimental.decode.decoder(
     state,
     chisel3.util.experimental.decode.TruthTable.fromString(
-      s"""???1->11000100110
-         |??1?->00101001001
-         |?1??->11000010110
-         |1???->00011000000
-         |      ???????????""".stripMargin
+      s"""00001->100000000000
+         |00010->001100100011
+         |00100->010010010100
+         |01000->001101000011
+         |10000->010010011000
+         |      ????????????""".stripMargin
     )
   )
+// readData
+// keepD ## keepC ## keepB ## keepA
+// updateDShift8 ## updateDShift16
+// updateC
+// updateBShift7 ## updateBShift12
+// updateA
+// flag
+// r dcba  d c  b a f
+// 1 0000 00 0 00 0 0
+// 0 0110 01 0 00 1 1
+// 0 1001 00 1 01 0 0
+// 0 0110 10 0 00 1 1
+// 0 1001 00 1 10 0 0
   assert(rounds < 20.U, "rounds should less then 20")
   val flag:           Bool = control(0)
   val updateA:        Bool = control(1)
@@ -60,56 +79,125 @@ class ChaCha(parameter: ChaChaParameter) extends Module {
   val keepB:          Bool = control(8)
   val keepC:          Bool = control(9)
   val keepD:          Bool = control(10)
+  val readData:       Bool = control(11)
 
-  val permuteOdd:     Bool = rounds(0) && state(3)
-  val permuteEven:    Bool = !rounds(0) && state(3)
+  val permuteOdd:  Bool = rounds(0)
+  val permuteEven: Bool = !rounds(0)
 
   // Data path
-  matrix.foreach { row =>
-    val a = row(0)
-    val b = row(1)
-    val c = row(2)
-    val d = row(3)
-    val adder = Module(new Add)
-    val xor = Module(new Xor)
-    adder.a := Mux(flag, a, c)
-    adder.b := Mux(flag, b, d)
-    xor.a := Mux(flag, d, b)
-    xor.b := adder.c
-    a := Mux1H(
-      Map(
-        updateA -> adder.c,
-        permuteOdd -> ???, // matrix()()
-        permuteEven -> ???, // matrix()()
-        keepA -> a
+  matrix.zipWithIndex.foreach {
+    case (row, i) =>
+      val a = row(0)
+      val b = row(1)
+      val c = row(2)
+      val d = row(3)
+      val adder = Module(new Add)
+      val xor = Module(new Xor)
+      adder.a := Mux(flag, a, c)
+      adder.b := Mux(flag, b, d)
+      xor.a := Mux(flag, d, b)
+      xor.b := adder.c
+      a := Mux1H(
+        Map(
+          readData -> Mux(
+            rounds === 0.U,
+            Mux1H(
+              Map(
+                (i == 0).B -> chacha_head_1,
+                (i == 1).B -> chacha_head_2,
+                (i == 2).B -> chacha_head_3,
+                (i == 3).B -> chacha_head_4
+              )
+            ),
+            matrix(i)(0)
+          ),
+          updateA -> adder.c,
+          keepA -> a
+        )
       )
-    )
-    b := Mux1H(
-      Map(
-        updateBShift12 -> xor.c.rotateLeft(12),
-        updateBShift7 -> xor.c.rotateLeft(7),
-        permuteOdd -> ???, // matrix()()
-        permuteEven -> ???, // matrix()()
-        keepB -> b
+      b := Mux1H(
+        Map(
+          readData -> Mux(
+            rounds === 0.U,
+            Mux1H(
+              Map(
+                (i == 0).B -> key(0)(0),
+                (i == 1).B -> key(0)(1),
+                (i == 2).B -> key(0)(2),
+                (i == 3).B -> key(0)(3)
+              )
+            ),
+            Mux1H(
+              Map(
+                permuteOdd -> matrix((i + 1) % 4)(1),
+                permuteEven -> matrix((i + 3) % 4)(1)
+              )
+            )
+          ),
+          updateBShift12 -> xor.c.rotateLeft(12),
+          updateBShift7 -> xor.c.rotateLeft(7),
+          keepB -> b
+        )
       )
-    )
-    c := Mux1H(
-      Map(
-        updateC -> adder.c,
-        permuteOdd -> ???, // matrix()()
-        permuteEven -> ???, // matrix()()
-        keepC -> c
+      c := Mux1H(
+        Map(
+          readData -> Mux(
+            rounds === 0.U,
+            Mux1H(
+              Map(
+                (i == 0).B -> key(1)(0),
+                (i == 1).B -> key(1)(1),
+                (i == 2).B -> key(1)(2),
+                (i == 3).B -> key(1)(3)
+              )
+            ),
+            Mux1H(
+              Map(
+                permuteOdd -> matrix((i + 2) % 4)(2),
+                permuteEven -> matrix((i + 2) % 4)(2)
+              )
+            )
+          ),
+          updateC -> adder.c,
+          keepC -> c
+        )
       )
-    )
-    d := Mux1H(
-      Map(
-        updateDShift16 -> xor.c.rotateLeft(16),
-        updateDShift8 -> xor.c.rotateLeft(8),
-        permuteOdd -> ???, // matrix()()
-        permuteEven -> ???, // matrix()()
-        keepD -> d
+      d := Mux1H(
+        Map(
+          readData -> Mux(
+            rounds === 0.U,
+            Mux1H(
+              Map(
+                (i == 0).B -> last_row(127, 127 - 32 + 1),
+                (i == 1).B -> last_row(127 - 32, 127 - 64 + 1),
+                (i == 2).B -> last_row(127 - 64, 127 - 96 + 1),
+                (i == 3).B -> last_row(127 - 96, 0)
+              )
+            ),
+            Mux1H(
+              Map(
+                permuteOdd -> matrix((i + 3) % 4)(3),
+                permuteEven -> matrix((i + 1) % 4)(3)
+              )
+            )
+          ),
+          updateDShift16 -> xor.c.rotateLeft(16),
+          updateDShift8 -> xor.c.rotateLeft(8),
+          keepD -> d
+        )
       )
-    )
+  }
+
+  // output
+  output.bits.nonce := nonce
+  output.bits.counter := counter
+  output.valid := (rounds === 19.U) && state(4).asBool
+  matrix.zipWithIndex.foreach { // the output shoule be reorgnized
+    case (row, i) =>
+      output.bits.x(i)(0) := matrix((4 - i) % 4)(i)
+      output.bits.x(i)(1) := matrix((5 - i) % 4)(i)
+      output.bits.x(i)(2) := matrix((6 - i) % 4)(i)
+      output.bits.x(i)(3) := matrix((7 - i) % 4)(i)
   }
 }
 
