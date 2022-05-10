7b5e816a1cf9ab1e9be3a60e6f3748f40f9d05aa
“âwissy <1536771081@qq.com>
2022-05-11 01:03:23 +0800

    srt4 fix

--- arithmetic/tests/src/division/srt/SRT4Test.scala
+++ arithmetic/tests/src/division/srt/SRT4Test.scala
@@ -8,13 +8,12 @@ object SRT4Test extends TestSuite with ChiselUtestTester{
   def tests: Tests = Tests {
     test("SRT4 should pass") {
       // parameters
-      val dividendWidth: Int = 8
-      val dividerWidth: Int  = 8
+      val dividendWidth: Int = 7
+      val dividerWidth: Int  = 7
       val n: Int = 10
       val dividend: Int = 15 << 3
       val divider:  Int = 3 << 5
       val counter: Int = 2
-//      val counter: Int = 1
       val quotient: Int = dividend / divider
       val remainder:  Int = dividend % divider
       // test
@@ -22,12 +21,10 @@ object SRT4Test extends TestSuite with ChiselUtestTester{
         Seq(chiseltest.internal.NoThreadingAnnotation,
         chiseltest.simulator.WriteVcdAnnotation)){
           dut: SRT =>
-//          dut.clock.setTimeout(0)
+          dut.clock.setTimeout(0)
           dut.input.valid.poke(true.B)
-          dut.input.bits.dividend.poke("b01111000".U)
-          dut.input.bits.divider.poke( "b01100000".U)
-//            dut.input.bits.dividend.poke("b01111000".U)
-//            dut.input.bits.divider.poke( "b01100000".U)
+          dut.input.bits.dividend.poke("b1111000".U)
+          dut.input.bits.divider.poke( "b1100000".U)
           dut.input.bits.counter.poke(counter.U)
           dut.clock.step()
           dut.input.valid.poke(false.B)
@@ -35,13 +32,11 @@ object SRT4Test extends TestSuite with ChiselUtestTester{
           for(a <- 1 to 20 if !flag) {
             if(dut.output.valid.peek().litValue == 1) {
               flag = true
-              dut.clock.step()
-              dut.output.bits.quotient.expect(0.U)
-              dut.output.bits.reminder.expect("b01111000".U)
-//              dut.output.bits.quotient.expect(1.U)
-//              dut.output.bits.reminder.expect("b11000".U)
+              dut.output.bits.quotient.expect(5.U)
+              dut.output.bits.reminder.expect(0.U)
             }
-            dut.clock.step()
+            else
+              dut.clock.step()
           }
             utest.assert(flag)
       }
