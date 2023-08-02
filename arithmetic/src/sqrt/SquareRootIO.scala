295e713a323c3e4476b8aa2416e41143f660eb38
Yanqi Yang <shxxyyq0000@qq.com>
2023-08-02 12:01:40 +0800

    fix sqrt QDS table

--- arithmetic/src/sqrt/SquareRootIO.scala
+++ arithmetic/src/sqrt/SquareRootIO.scala
@@ -12,3 +12,14 @@ class SquareRootInput(inputWidth: Int, outputWidth: Int) extends Bundle{
 class SquareRootOutput(outputWidth: Int) extends Bundle{
   val result = UInt((outputWidth).W)
 }
+
+class QDSInput(rWidth: Int, partialDividerWidth: Int) extends Bundle {
+  val partialReminderCarry: UInt = UInt(rWidth.W)
+  val partialReminderSum:   UInt = UInt(rWidth.W)
+  /** truncated divisor without the most significant bit  */
+  val partialDivider: UInt = UInt(partialDividerWidth.W)
+}
+
+class QDSOutput(ohWidth: Int) extends Bundle {
+  val selectedQuotientOH: UInt = UInt(ohWidth.W)
+}
