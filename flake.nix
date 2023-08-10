8e0f4a703c0ffbda5990ec21f2d8bcf01869bf11
Jiuyang Liu <liu@jiuyang.me>
2023-08-10 23:47:03 +0800

    add nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "arithmetic";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+    let
+      overlay = import ./overlay.nix;
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+          deps = with pkgs; [
+            git
+            mill
+            circt
+            verilator
+          ];
+        in
+        {
+          legacyPackages = pkgs;
+          devShell = pkgs.mkShell {
+            buildInputs = deps;
+          };
+        }
+      )
+    // { inherit inputs; overlays.default = overlay; };
+}
