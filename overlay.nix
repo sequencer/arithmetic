65cdcc86dc8b40d55d900c3152d539cbfe9748fd
Avimitin <dev@avimit.in>
2025-01-26 00:15:35 +0800

    [nix] override mill to a working version

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,18 @@
 final: prev: {
+  fetchMillDeps = final.callPackage ./nix/fetch-mill-deps.nix { inherit (final.xorg) lndir; };
+  publishMillJar = final.callPackage ./nix/publish-mill-jar.nix { inherit (final.xorg) lndir; };
+  configure-mill-home-hook = final.callPackage ./nix/configure-mill-home.nix { };
+
+  chisel = final.callPackage ./nix/chisel.nix { };
+
+  mill = prev.mill.overrideAttrs {
+    version = "unstable-0.12.5-173-15dded";
+    src = final.fetchurl {
+      url = "https://github.com/com-lihaoyi/mill/releases/download/0.12.5/0.12.5-173-15dded-assembly";
+      hash = "sha256-xP59tONOu0CG5Gce4ru+st5KUH7Wcd10d/pQdELjSJM=";
+    };
+  };
+
   espresso = final.callPackage ./nix/espresso.nix { };
   softfloat = final.callPackage ./nix/softfloat.nix { };
   testfloat = final.callPackage ./nix/testfloat.nix { };
